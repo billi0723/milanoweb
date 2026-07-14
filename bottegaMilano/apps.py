c486f4774037c6f9ac26a559476c14e7127274f9
Guillermo William <billimilano23@gmail.com>
2026-07-14 06:54:44 +0200

    app

--- bottegaMilano/apps.py
+++ bottegaMilano/apps.py
@@ -5,7 +5,3 @@ import os
 class BottegamilanoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bottegaMilano'
-
-
-<h4 style="color:green">{{num}}</h4>
-<pre style="color:darkslategrey; padding:40px; text-size-adjust:initial; white-space:pre-wrap; word-wrap:break-word">{{testo|safe}}</pre>
